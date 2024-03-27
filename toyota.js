b1f91d93f7631920bd8e359ed27458a1c52cd0ad
hussnain314 <162672994+hussnain314@users.noreply.github.com>
2024-03-27 13:36:31 +0500

    Add files via upload

---
+++ toyota.js
@@ -0,0 +1,13 @@
+let toyota = {
+    name: "black",
+    price: "50 lacs",
+    model: 2023,
+    miles: 30,
+
+}
+let carname = toyota.name;
+let carprice = toyota.price;
+let carmodel = toyota.model;
+let carmiles = toyota.miles;
+
+console.log(carname , carprice , carmodel , carmiles);
